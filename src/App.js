a60fee70b3836c115d28947ffbccd1ce59625dc7
Benjamin Rose <benjaminrose@Benjamins-MacBook-Air.local>
2020-03-22 14:14:40 -0400

    update 3/22/2020 2:15pm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
@@ -25,7 +25,9 @@ class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <LoginForm />
+        <SafeAreaView>
+          <LoginForm />
+        </SafeAreaView>
       </Provider>
     );
   }
